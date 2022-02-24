5f7f0fc15d3c1f82b6c65aecdb643aa04323a916
Darimo4ka <darima_badm@yahoo.com>
2022-02-24 09:32:57 -0500

    api routes set and all working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,23 @@
-//  create connection to this folder
+// //  create connection to this folder
+const path = require('path');
+
+// req esxpress router
+const router = require('express').Router()
+
+
+router.get("/api/notes", (req, res) => {
+
+    // 'utf8' what is it good practice to include?
+    fs.readFile(path.join(__dirname, "db", "db.json"), (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
+        }
+        console.log('File data:', jsonString)
+        response.json(JSON.parse(jsonString));
+    })
+})
+
+//   export 
+module.exports = router;
+
--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 
+// configuration statment config the public folder to read from it static content
+app.use(express.static('public')); 
 
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// create connection to our routes folders
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// create connection to our routes folders. this is routes defined if i recive req at the end of it slash / it will go to this difen and find all the slashes and proceed to do w/e that an. fnc
 
-// start server
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+// start server.this is where we start our server on this port
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
